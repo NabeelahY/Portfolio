74ac763dd88db368246ed5a4536880e2120d551b
Nabeelah <noblenab13@gmail.com>
2019-10-22 07:55:35 +0100

    Add footer

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterStyle } from './styles/FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterStyle>
+      <p>2019 &copy; Designed by Nabeelah Yousuph</p>
+    </FooterStyle>
+  );
+};
+
+export default Footer;
---
+++ portfolio/src/components/styles/FooterStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterStyle = styled.footer`
+  @import url('https://fonts.googleapis.com/css?family=Josefin+Sans:300,400&display=swap');
+  border-top: 1px solid #fff;
+  text-align center;
+  width: 1200px;
+  font-size: 20px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 300;
+  color: #fff;
+  margin: auto auto 0;
+  padding: 20px 0;
+`;
